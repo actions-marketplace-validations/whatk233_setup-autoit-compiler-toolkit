98be92a13e20f428719ae1d20c630b09670f40f9
Whatk <whatk@foxmail.com>
2023-01-16 21:49:15 +0800

    feat: update toolkit download url

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$Toolkit_Download_Url="https://github.com/whatk233/setup-autoit-compiler-toolkit/releases/download/3.3.16.1_230116/autoit_compiler_toolkit_v3.3.16.1_230116.zip"
+$Toolkit_Download_Url="https://github.com/whatk233/setup-autoit-compiler-toolkit/releases/download/toolkit/autoit_compiler_toolkit_v3.3.16.1_230116.zip"
 $Toolkit_Download_Path="$env:TEMP\autoit_compiler_toolkit.zip"
 $Toolkit_Path="$env:APPDATA\autoit_compiler_toolkit"
 
